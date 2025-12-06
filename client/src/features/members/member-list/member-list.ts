c3cebfde41b2a1c7191dfada34a0f88ce4b6ba66
ana <anaionita.fly@gmail.com>
2025-12-06 17:38:36 +0200

    add routing, toasts, route guard

---
+++ client/src/features/members/member-list/member-list.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-member-list',
+  imports: [],
+  templateUrl: './member-list.html',
+  styleUrl: './member-list.css'
+})
+export class MemberList {
+
+}
